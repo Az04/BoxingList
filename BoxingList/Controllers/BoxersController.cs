fa2f3fbe232b89239638c46c3c25c022cd8b25ec
Anell Zheng <anellzheng_04@hotmail.com>
2019-05-16 03:06:31 -0500

    Create and show all data from DB Edit action and show changes

--- BoxingList/Controllers/BoxersController.cs
+++ BoxingList/Controllers/BoxersController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using BoxingList.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace BoxingList.Controllers
 {
@@ -26,6 +27,52 @@ namespace BoxingList.Controllers
         {
             //Load container for name, division and record
             return View();
+        }
+
+        //POST CREATE BOXER
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Boxer boxer)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Add(boxer);
+                await _db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(boxer);
+        }
+
+        //GET EDIT BOXER INFO
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var boxer = await _db.Boxers.SingleOrDefaultAsync(m => m.Id == id);
+
+            if (boxer == null)
+            {
+                return NotFound();
+            }
+
+            return View(boxer);
+        }
+
+        //POST EDIT BOXER INFO
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(Boxer boxer)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Update(boxer);
+                await _db.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(boxer);
         }
     }
 }
